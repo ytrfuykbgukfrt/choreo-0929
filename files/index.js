29c9aecbf986377aa14fe280fe42ee2d81fd6863
ytrfuykbgukfrt <155209975+ytrfuykbgukfrt@users.noreply.github.com>
2024-09-30 23:45:11 -0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,8 +6,8 @@ const UUID = process.env.UUID || '74a3e866-c437-4998-92c7-f5c24a3f447d'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || 'N9BVoBfucVIrIzCBt8';   // 没用哪吒，把这个参数空着
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'hoost02.lookmondike.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNWVmN2IzYzc2YmExMWFjOWY3MzYwYTcxYmEzZTgwMzIiLCJ0IjoiMmQ2NGExNjItMjQxMS00NTkzLThiMjUtZWU1N2I1MTk0ZTAyIiwicyI6Ik9HSXhZMkk0TVRjdFlUTTBZUzAwWXpKakxXRmtOVEl0TURRelptSXdabVl4WW1WaCJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'hedxjyki.lookmondike.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNWVmN2IzYzc2YmExMWFjOWY3MzYwYTcxYmEzZTgwMzIiLCJ0IjoiMDAyOGZlMWMtZjQ5MC00NzA0LTliY2QtYzg5ZjM3ZjI2MGEyIiwicyI6Ik1HTmpNRFEzTXpFdE56RTJZUzAwTWpreUxUaG1aVGN0TUROalpXRTVNMk15T0RBMSJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
